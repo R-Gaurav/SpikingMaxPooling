13bf55e652778cad3f0f618ec4e16f25b727acdb
Ramashish Ramashish Gaurav <rgaurav@gra-login2.graham.sharcnet>
2021-09-06 00:19:01 -0400

    Commit

--- utils/cnn_2d_utils.py
+++ utils/cnn_2d_utils.py
@@ -6,7 +6,8 @@ from utils.base_utils import log
 
 import tensorflow as tf
 
-def _get_2d_cnn_block(block, data_format, padding, layer_cfg, layer_objs_lst):
+def _get_2d_cnn_block(block, data_format, padding, layer_cfg, layer_objs_lst,
+                      use_bias=False):
   """
   Returns a conv block.
 
@@ -25,7 +26,7 @@ def _get_2d_cnn_block(block, data_format, padding, layer_cfg, layer_objs_lst):
   conv = tf.keras.layers.Conv2D(
     layer_cfg.num_kernels, layer_cfg.kernel_dims, strides=layer_cfg.stride_dims,
     data_format=data_format, activation="relu", padding=padding,
-    use_bias=False)(block)
+    use_bias=use_bias)(block)
   layer_objs_lst.append(conv)
 
   return conv
@@ -118,13 +119,15 @@ def get_2d_cnn_model(inpt_shape, tf_cfg, num_clss=10, include_dropout=True):
   model = tf_cfg["tf_model"]
   data_format = "channels_first" if tf_cfg["is_channels_first"] else "channels_last"
   padding="same" if tf_cfg["tf_model"]["name"] == "model_7" else "valid"
+  use_bias=True if tf_cfg["tf_model"]["name"] == "model_7" else False
 
   ###################### Construct the model's arch. ########################
   # Add Conv and MaxPool blocks.
   x = inpt_lyr
   for _, layer in model["layers"].items(): # Dicts are ordered in Python3.7.
     if layer.name == "Conv":
-      x = _get_2d_cnn_block(x, data_format, padding, layer, layer_objs_lst)
+      x = _get_2d_cnn_block(
+          x, data_format, padding, layer, layer_objs_lst, use_bias=use_bias)
     elif layer.name == "MaxPool":
       x = _get_max_pool_block(x, data_format, "valid", layer, layer_objs_lst)
     elif layer.name == "AvgPool":
